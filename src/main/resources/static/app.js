3bc46d8a4c3bc9585fe0e574d2fca0cf4e717f22
gyoungsubLee <dltjq2323@gmail.com>
2025-01-04 14:55:12 +0900

    feat: 클라이언트 코드

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,61 @@
+const stompClient = new StompJs.Client({
+    brokerURL: 'ws://localhost:8080/websocketConnection'
+});
+
+stompClient.onConnect = (frame) => {
+    setConnected(true);
+    console.log('Connected: ' + frame);
+    stompClient.subscribe('/topic/messaging', (message) => {
+        showMessages(JSON.parse(message.body).content);
+    });
+};
+
+stompClient.onWebSocketError = (error) => {
+    console.error('Error with websocket', error);
+};
+
+stompClient.onStompError = (frame) => {
+    console.error('Broker reported error: ' + frame.headers['message']);
+    console.error('Additional details: ' + frame.body);
+};
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#messages").html("");
+}
+
+function connect() {
+    stompClient.activate();
+}
+
+function disconnect() {
+    stompClient.deactivate();
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendMessage() {
+    stompClient.publish({
+        destination: "/app/hello",
+        body: JSON.stringify({'content': $("#name").val()})
+    });
+}
+
+function showMessages(message) {
+    $("#messages").append("<tr><td>" + message + "</td></tr>");
+}
+
+$(function () {
+    $("form").on('submit', (e) => e.preventDefault());
+    $( "#connect" ).click(() => connect());
+    $( "#disconnect" ).click(() => disconnect());
+    $( "#send" ).click(() => sendMessage());
+});
+
